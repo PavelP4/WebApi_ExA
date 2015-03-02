704889d40ba8f20e306d0ec76617ae118d7712d8
PavelP4 <reginuser@gmail.com>
2015-03-02 09:53:00 +0300

    Initial commit

---
+++ Src/MvcApp1/MvcApp1/Controllers/AutoController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace MvcApp1.Controllers
+{
+    public class AutoController : ApiController
+    {
+        public string GetOK()
+        {
+            return "Auto OK!";
+        }
+    }
+}
