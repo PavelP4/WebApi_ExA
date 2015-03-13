f2320d0049e43d8c9f576b5b4e9a37ce4a929a96
PavelP4 <reginuser@gmail.com>
2015-03-13 17:08:06 +0300

    +

---
+++ Src/MvcApp1/MvcApp1/Controllers/V2/PushController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading;
+using System.Web.Http;
+
+namespace MvcApp1.Controllers.V2
+{
+    public class PushController : ApiController
+    {
+        private static readonly Lazy<Timer> timer = new Lazy<Timer>(() => new Timer(TimerCallback, null, 0, 2000));
+
+        private static readonly ConcurrentDictionary<StreamWriter, StreamWriter> subscriptions =        
+            new ConcurrentDictionary<StreamWriter, StreamWriter>();
+
+        public HttpResponseMessage Get()
+        {
+            Timer t = timer.Value;
+            
+            Request.Headers.AcceptEncoding.Clear();
+            HttpResponseMessage response = Request.CreateResponse();
+            response.Headers.Add("Access-Control-Allow-Origin", "*");
+            response.Content = new PushStreamContent(OnStreamAvailable, "text/event-stream");
+            
+            return response;
+        }
+
+        private static void OnStreamAvailable(Stream stream, HttpContent headers,
+            TransportContext context)
+        {
+            StreamWriter writer = new StreamWriter(stream);
+            subscriptions.TryAdd(writer, writer);
+        }
+    }
+}
---
+++ Src/MvcApp1/MvcApp1/Controllers/V2/TestController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace MvcApp1.Controllers.V2
+{
+    public class TestController : ApiController
+    {
+        private const int delay = 500;
+
+        public string GetString(int id)
+        {      
+            using (StreamReader reader = File.OpenText(@"d:\Temp\testFile.log"))
+            {
+                Thread.Sleep(delay);
+
+                return reader.ReadToEnd();
+            }
+        }
+
+        public async Task<string> GetString()
+        {
+            return await ReadFileAsync();
+        }
+
+        private async Task<string> ReadFileAsync()
+        {
+            using (StreamReader reader = File.OpenText(@"d:\Temp\testFile.log"))
+            {
+                await Task.Delay(delay);
+
+                return await reader.ReadToEndAsync();
+            }        
+        }
+
+    }
+}
